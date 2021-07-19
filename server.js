adeffeff7c3bd3f799d937116b43f82483d9da1d
Samo <paulsamo1234@gmail.com>
2021-07-19 20:12:37 +0530

    inital author routes

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ const expressLayouts = require('express-ejs-layouts')
 
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author') 
 
 app.set('view engine',"ejs")
 app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.DATABASE_URI,{
@@ -31,6 +34,7 @@ console.log("Connection not Successful")
 
 
 app.use("/",indexRouter)
+app.use("/authors", authorRouter)
 
 
 // const port = process.env.PORT || 3000;
